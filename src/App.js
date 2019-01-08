88615692a06c64f0ab7c2105845ee19cfe401c45
radmilatomic <radmila.tomic.rt@gmail.com>
2019-01-08 17:21:12 +0100

    added song component within App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+import Song from './components/song'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div>ovo je moja playlista</div>
+        <Song/>
+        <Song/>
+        <Song/>
+
       </div>
     );
   }
---
+++ src/components/song/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Song=()=>{
+	return(
+		<div>Jedna pesma</div>
+	)
+}
+
+export default Song
