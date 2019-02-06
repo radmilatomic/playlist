b0fd93cdbb30af17c225c0e90266b11ff07efaf0
radmilatomic <radmila.tomic.rt@gmail.com>
2019-02-06 17:25:54 +0100

    enabled adding songs

--- src/components/addSong/index.js
+++ src/components/addSong/index.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const AddSong=(props)=>{
+class AddSong extends Component{
+
+	fetchSongs(){
+		console.log("song should be added");
+	}
+
+	addSong(e){
+		console.log("song will be added here");
+		e.preventDefault()
+    var form=new FormData(document.getElementById('form'))
+   const url=new URL('https://radmilatomic.pythonanywhere.com/api/addsong')
+   const request=new Request(url,{
+    method:'POST',
+    body:form,
+    mode:'cors'
+   });
+   fetch(request).then(()=>this.fetchSongs())
+     .catch(function(error){console.log(error);})
+
+  
+	}
+	render(){
 	return(
-		<div>
-		this is a Add Song Component
-		</div>
+		<form  id="form">
+		Title:<input name="title"></input>
+		Performer:<input name="performer"></input>
+		<input  type ="submit" value ="Add" onClick={this.addSong}/>
+		</form>
 
 	)
+}
 }
 
 export default AddSong
