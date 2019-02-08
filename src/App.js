16a3c7243eb624238fc92f517ae5f7d6bf8b666b
radmilatomic <radmila.tomic.rt@gmail.com>
2019-02-08 09:16:23 +0100

    docs folder added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>ovo je moja playlista</div>
+        <div>ovo je moja playlista, al dodaj i ti nesto slobodno neku pesmu</div>
        <AddSong/>
        <SongList/>
         
---
+++ src/components/deleteModal/index.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+class deleteModal extends Component{
+	render(){
+		return <div>ovo je modal, svasta treba ovde implementirati</div>
+	}
+}
